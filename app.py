925a4b4d9bdb9562d666d62f7de5a53ea3ca3f70
guiyote81 <guiyote81@hotmail.com>
2026-09-13 18:17:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
 def home():
 return "Servidor TradingView cTrader funcionando"
 
-@app.route("/callback")
+app.add_url_rule("/", "home", home)
+
 def callback():
 return "Callback cTrader funcionando"
 
-@app.route("/webhook", methods=["POST"])
+app.add_url_rule("/callback", "callback", callback)
+
 def webhook():
 return "Webhook recibido", 200
+
+app.add_url_rule("/webhook", "webhook", webhook, methods=["POST"])
