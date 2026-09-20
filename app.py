b9799c7c20281d547ebb8fdfe9b529e994042657
guiyote81 <guiyote81@hotmail.com>
2026-09-19 23:40:51 -0300

    Refactor webhook handling into a separate function

--- app.py
+++ app.py
@@ -3,22 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-app.add_url_rule(
-"/",
-"home",
-lambda: "Servidor funcionando correctamente"
-)
-
-
-app.add_url_rule(
-"/status",
-"status",
-lambda: "OK"
-)
-
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
+def recibir_webhook():
 mensaje = request.get_data(as_text=True)
 
 print("======================================")
@@ -27,3 +12,23 @@ print("Mensaje:", mensaje)
 print("======================================")
 
 return "Webhook recibido correctamente", 200
+
+
+app.add_url_rule(
+"/",
+"home",
+lambda: "Servidor funcionando correctamente"
+)
+
+app.add_url_rule(
+"/status",
+"status",
+lambda: "OK"
+)
+
+app.add_url_rule(
+"/webhook",
+"webhook",
+recibir_webhook,
+methods=["POST"]
+)
